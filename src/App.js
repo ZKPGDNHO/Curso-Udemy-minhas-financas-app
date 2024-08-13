dd75e6936b72ac6bb976e93f6da5ec83ec71281e
ZKPGDNHO <pedrohs.sxntos@gmail.com>
2024-08-13 19:52:25 -0300

    Iniciando a parte Front-end

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './views/Login';
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
+class App extends React.Component {
+  
+    render() {
+      return(
+      <>
+      <Login/>
+      </>
+      )
+    }
+}
+
+export default App;
